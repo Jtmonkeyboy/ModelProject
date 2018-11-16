1e9dccfe5076056662ffd43574cddff24d5b00d6
Jtmonkeyboy <43076688+Jtmonkeyboy@users.noreply.github.com>
2018-11-16 12:34:01 -0700

    I added in an array.

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -5,12 +5,18 @@ import javax.swing.JOptionPane;
 import model.Guitar;
 
 import java.util.ArrayList;
+//To make the ImageIcon array, you must give this import statement.
+import javax.swing.ImageIcon;
 
 public class Controller
 {
 	private Guitar myGuitar;
 	private Guitar userGuitar;
 	private ArrayList<Guitar> guitarList = new ArrayList<Guitar>();
+	
+	//Initialization of the arrays.
+	private Guitar[]  guitarArray;
+	private ImageIcon[] icons;
 	
 	public Controller()
 	{
@@ -21,13 +27,23 @@ public class Controller
 	public void start()
 	{
 		buildTheLists();
+		arrays();
 		JOptionPane.showMessageDialog(null, myGuitar);
 	}
 	
+	//This method is called to create the array lists.
 	public void buildTheLists()
 	{
 		guitarList.add(myGuitar);
 		guitarList.add(myGuitar);
 		guitarList.add(userGuitar);
+	}
+	
+	//This method is called to create the arrays.
+	public void arrays()
+	{
+		icons = new ImageIcon[3];
+		
+		guitarArray = new Guitar[3];
 	}
 }
